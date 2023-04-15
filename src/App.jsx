d7bd3287a4bb7d1dfb804415b77931475e316c52
Sebastian Peralta <sebastian.peralta1@takeda.com>
2023-04-15 15:45:50 -0500

    feat: new form

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFoundPage";
 import Header from "./components/Header";
 import Contain from "./components/Contain";
+import ResultsPage from "./pages/ResultsPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ProcessContextProvider from "./context/ProcessContext";
 function App() {
   return (
     <>
       <ToastContainer />
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/roundrobin" element={<Contain />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+        <ProcessContextProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/roundrobin" element={<Contain />}></Route>
+            <Route path="/results" element={<ResultsPage />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </ProcessContextProvider>
       </Router>
     </>
   );
